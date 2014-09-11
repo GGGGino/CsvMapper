95a240b6b17da644667e818024ba1d8d8c9d2a9a
ggggino <david.ginanni@gmail.com>
2014-09-11 19:51:39 +0200

    corretto test

--- tests/Ggggino/CsvMapperTest.php
+++ tests/Ggggino/CsvMapperTest.php
@@ -7,8 +7,7 @@ class MoneyTest extends PHPUnit_Framework_TestCase
     public function testCanBeNegated()
     {
         $obj = new CsvMapper("../../example/sample2.csv");
-        print_r($obj->stampCsv());
-        print_r($obj->getBy("nome", 'David'));
+        print_r($obj->getCsv());
     }
 
     // ...
